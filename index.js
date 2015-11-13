824036932a8c9a02a3cd935aa3447c5dfb51e5a5
James Talmage <james@talmage.io>
2015-11-12 19:26:26 -0500

    add delay after promise resolves

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 'use strict';
 module.exports = function (ms) {
-	return new Promise(function (resolve) {
-		setTimeout(resolve, ms || 0);
+	ms = ms || 0;
+
+	var promise = new Promise(function (resolve) {
+		setTimeout(resolve, ms);
 	});
+
+	function thunk(result) {
+		return new Promise(function (resolve) {
+			setTimeout(function () {
+				resolve(result);
+			}, ms);
+		});
+	}
+
+	thunk.then = promise.then.bind(promise);
+
+	return thunk;
 };
