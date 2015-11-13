a9ad91b6c4a63ef8a8f7859c5816ff3225afa696
James Talmage <james@talmage.io>
2015-11-13 11:11:00 +0700

    Close #3 PR: add delay after promise resolves. Fixes #4

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 import test from 'ava';
 import fn from './';
 
-test(async t => {
+test('promise', async t => {
 	const date = Date.now();
 	await fn(50);
 	const diff = Date.now() - date;
 	t.true(diff > 30 && diff < 70);
+});
+
+test('thunk', async t => {
+	const date = Date.now();
+	var result = await Promise.resolve('foo').then(fn(50));
+	const diff = Date.now() - date;
+	t.true(diff > 30 && diff < 80);
+	t.is(result, 'foo');
 });
