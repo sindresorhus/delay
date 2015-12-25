74c7de89d21336d946a42ea01b51244dac0127af
Ari Porad <ari@ariporad.com>
2015-12-25 22:47:13 +0100

    Add `delay.reject()`
    closes #8
    fixes #7

--- test.js
+++ test.js
@@ -6,12 +6,38 @@ import fn from './';
 test('promise', async t => {
 	const end = timeSpan();
 	await fn(50);
-	t.true(inRange(end(), 30, 70));
+	t.true(inRange(end(), 30, 70), 'is delayed');
 });
 
 test('thunk', async t => {
 	const end = timeSpan();
 	const result = await Promise.resolve('foo').then(fn(50));
-	t.true(inRange(end(), 30, 70));
-	t.is(result, 'foo');
+	t.true(inRange(end(), 30, 70), 'is delayed');
+	t.is(result, 'foo', 'passes through the value');
+});
+
+test('.reject() with two arguments', async t => {
+	const end = timeSpan();
+	await t.throws(fn.reject(50, new Error('foo')), 'foo', 'promise is rejected with the second argument');
+	t.true(inRange(end(), 30, 70), 'is delayed');
+});
+
+test('.reject() with two arguments (as a thunk)', async t => {
+	const end = timeSpan();
+	await t.throws(
+		Promise.resolve('foo').then(fn.reject(50, new Error('bar'))),
+		'bar',
+		'promise is rejected with the resolution value'
+	);
+	t.true(inRange(end(), 30, 70), 'is delayed');
+});
+
+test('.reject() with one argument (thunk)', async t => {
+	const end = timeSpan();
+	await t.throws(
+		Promise.resolve(new Error('foo')).then(fn.reject(50)),
+		'foo',
+		'promise is rejected with the resolution value'
+	);
+	t.true(inRange(end(), 30, 70), 'is delayed');
 });
