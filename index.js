0b0d22a0198980d0d235bb35413204215aa2a95f
Tim De Pauw <timdp@users.noreply.github.com>
2017-02-05 14:03:33 +0100

    ES2015ify (#16)

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 'use strict';
 
-function generate(willResolve) {
-	return function (ms, value) {
-		ms = ms || 0;
+const generate = willResolve => function (ms, value) {
+	ms = ms || 0;
 
-		// If supplied, the thunk will override promise results with `value`.
-		var useValue = arguments.length > 1;
+	// If supplied, the thunk will override promise results with `value`.
+	const useValue = arguments.length > 1;
 
-		var promise = thunk(value);
-		thunk.then = promise.then.bind(promise);
-		thunk.catch = promise.catch.bind(promise);
-		thunk._actualPromise = promise;
-		return thunk;
+	let promise;
 
-		function thunk(result) {
-			if (promise) {
-				// Prevent unhandled rejection errors if promise is never used, and only used as a thunk
-				promise.catch(function () {});
-			}
-			return new Promise(function (resolve, reject) {
-				setTimeout(
-					willResolve ? resolve : reject,
-					ms,
-					useValue ? value : result
-				);
-			});
+	const thunk = result => {
+		if (promise) {
+			// Prevent unhandled rejection errors if promise is never used, and only used as a thunk
+			promise.catch(() => {});
 		}
+		return new Promise((resolve, reject) => {
+			setTimeout(
+				willResolve ? resolve : reject,
+				ms,
+				useValue ? value : result
+			);
+		});
 	};
-}
+
+	promise = thunk(value);
+	thunk.then = promise.then.bind(promise);
+	thunk.catch = promise.catch.bind(promise);
+	thunk._actualPromise = promise;
+
+	return thunk;
+};
 
 module.exports = generate(true);
 module.exports.reject = generate(false);
