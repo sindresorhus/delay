0204f6e740834a4f2d69e247a0fe2f04b8ff1e7a
Dmitry Korolev <dmitry@korolev.dk>
2018-08-20 10:49:22 +0300

    Add todo notice about reason type

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,33 @@
 interface ClearablePromise<T> extends Promise<T> {
- /**
-  * Clears the delay and settles the promise.
-  *
-  * @memberof ClearablePromise
-  */
-  clear(): void;
+	/**
+	 * Clears the delay and settles the promise.
+	 *
+	 * @memberof ClearablePromise
+	 */
+	clear(): void;
 }
 
 declare const delay: {
-  /**
-    * Create a promise which resolves after the specified `milliseconds`.
-    * Optionally pass a `value` to resolve.
-    *
-    * @param {number} milliseconds Milliseconds to delay the promise.
-    * @param {T} [value] Value to resolve in the returned promise.
-    * @returns {ClearablePromise<T>} a promise which resolves after the specified `milliseconds`
-    */
-  <T = never>(milliseconds: number, value?: T): ClearablePromise<T>;
-  
-  /**
-    * Create a promise which rejects after the specified `milliseconds`.
-    * Optionally pass a `reason` to reject.
-    *
-    * @param {number} milliseconds Milliseconds to delay the promise.
-    * @param {*} [reason] Value to reject in the returned promise.
-    * @returns {ClearablePromise<never>} a promise which rejects after the specified `milliseconds`.
-    */
-  reject(milliseconds: number, reason?: any): ClearablePromise<never>;
+	/**
+	 * Create a promise which resolves after the specified `milliseconds`.
+	 * Optionally pass a `value` to resolve.
+	 *
+	 * @param {number} milliseconds Milliseconds to delay the promise.
+	 * @param {T} [value] Value to resolve in the returned promise.
+	 * @returns {ClearablePromise<T>} a promise which resolves after the specified `milliseconds`
+	 */
+	<T = never>(milliseconds: number, value?: T): ClearablePromise<T>;
+
+	/**
+	 * Create a promise which rejects after the specified `milliseconds`.
+	 * Optionally pass a `reason` to reject.
+	 *
+	 * @param {number} milliseconds Milliseconds to delay the promise.
+	 * @param {*} [reason] Value to reject in the returned promise.
+	 * @returns {ClearablePromise<never>} a promise which rejects after the specified `milliseconds`.
+	 */
+	// TODO: Allow providing reason type after https://github.com/Microsoft/TypeScript/issues/5413 will be resolved.
+	reject(milliseconds: number, reason?: any): ClearablePromise<never>;
 };
 
 export default delay;
