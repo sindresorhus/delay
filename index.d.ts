d5a7a445f68545c7f6f875c8f2ffbd7256fd3b36
Dmitry Korolev <dmitry@korolev.dk>
2018-08-20 10:45:02 +0300

    Fix code style

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@ interface ClearablePromise<T> extends Promise<T> {
   *
   * @memberof ClearablePromise
   */
- clear(): void;
+  clear(): void;
 }
 
 declare const delay: {
@@ -15,17 +15,18 @@ declare const delay: {
     * @param {number} milliseconds Milliseconds to delay the promise.
     * @param {T} [value] Value to resolve in the returned promise.
     * @returns {ClearablePromise<T>} a promise which resolves after the specified `milliseconds`
-  */
+    */
   <T = never>(milliseconds: number, value?: T): ClearablePromise<T>;
- /**
-  * Create a promise which rejects after the specified `milliseconds`.
-  * Optionally pass a `reason` to reject.
-  *
-  * @param {number} milliseconds Milliseconds to delay the promise.
-  * @param {*} [reason] Value to reject in the returned promise.
-  * @returns {ClearablePromise<never>} a promise which rejects after the specified `milliseconds`.
-  */
- reject(milliseconds: number, reason?: any): ClearablePromise<never>;
+  
+  /**
+    * Create a promise which rejects after the specified `milliseconds`.
+    * Optionally pass a `reason` to reject.
+    *
+    * @param {number} milliseconds Milliseconds to delay the promise.
+    * @param {*} [reason] Value to reject in the returned promise.
+    * @returns {ClearablePromise<never>} a promise which rejects after the specified `milliseconds`.
+    */
+  reject(milliseconds: number, reason?: any): ClearablePromise<never>;
 };
 
 export default delay;
