69136a2dd61022a3e0eea31962d4daf368e3f315
Sindre Sorhus <sindresorhus@gmail.com>
2018-09-07 13:54:22 +0700

    Bump `tsd-check` dev dependency

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,12 +1,10 @@
 import {expectType} from 'tsd-check';
 import delay from '.';
 
-(async () => {
-	expectType<void>(await delay(200));
+expectType<void>(await delay(200));
 
-	expectType<string>(await delay(200, {value: '🦄'}));
-	expectType<number>(await delay(200, {value: 0}));
+expectType<string>(await delay(200, {value: '🦄'}));
+expectType<number>(await delay(200, {value: 0}));
 
-	expectType<never>(await delay.reject(200, {value: '🦄'}));
-	expectType<never>(await delay.reject(200, {value: 0}));
-})();
+expectType<never>(await delay.reject(200, {value: '🦄'}));
+expectType<never>(await delay.reject(200, {value: 0}));
