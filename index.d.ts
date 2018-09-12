2c8e4b1ade3b5168c5c1782918ac9b1031e49d73
Sam Verschueren <sam.verschueren@gmail.com>
2018-09-12 20:35:35 +0200

    Refactor the options interface

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 /// <reference lib="dom"/>
 
+type Omit<Interface, Properties extends keyof Interface> = Pick<Interface, Exclude<keyof Interface, Properties>>;
+
 export interface ClearablePromise<T> extends Promise<T> {
 	/**
 	 * Clears the delay and settles the promise.
@@ -7,12 +9,16 @@ export interface ClearablePromise<T> extends Promise<T> {
 	clear(): void;
 }
 
-export interface Options {
+export interface Options<Value = any> {
 	/**
 	 * An optional AbortSignal to abort the delay.
 	 * If aborted, the Promise will be rejected with an AbortError.
 	 */
-	signal?: AbortSignal
+	signal?: AbortSignal,
+	/**
+	 * Value to resolve in the returned promise.
+	 */
+	value: Value
 }
 
 declare const delay: {
@@ -22,7 +28,7 @@ declare const delay: {
 	 * @param milliseconds - Milliseconds to delay the promise.
 	 * @returns A promise which resolves after the specified `milliseconds`.
 	 */
-	(milliseconds: number, options?: Options): ClearablePromise<void>;
+	(milliseconds: number, options?: Omit<Options, 'value'>): ClearablePromise<void>;
 
 	/**
 	 * Create a promise which resolves after the specified `milliseconds`.
@@ -30,10 +36,7 @@ declare const delay: {
 	 * @param milliseconds - Milliseconds to delay the promise.
 	 * @returns A promise which resolves after the specified `milliseconds`.
 	 */
-	<T>(milliseconds: number, options?: Options & {
-		/** Value to resolve in the returned promise. */
-		value: T
-	}): ClearablePromise<T>;
+	<Value>(milliseconds: number, options?: Options<Value>): ClearablePromise<Value>;
 
 	/**
 	 * Create a promise which rejects after the specified `milliseconds`.
@@ -42,10 +45,7 @@ declare const delay: {
 	 * @returns A promise which rejects after the specified `milliseconds`.
 	 */
 	// TODO: Allow providing value type after https://github.com/Microsoft/TypeScript/issues/5413 will be resolved.
-	reject(milliseconds: number, options?: Options & {
-		/** Value to reject in the returned promise. */
-		value?: any
-	}): ClearablePromise<never>;
+	reject(milliseconds: number, options?: Options): ClearablePromise<never>;
 };
 
 export default delay;
