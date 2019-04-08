fa2d83cca16c5b8b04e95871908504a2f3ca3628
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-08 10:07:14 +0000

    Refactor TypeScript definition to CommonJS compatible export (#44)

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,19 +1,24 @@
-import {expectType} from 'tsd-check';
-import delay from '.';
+/// <reference lib="dom"/>
+import {expectType} from 'tsd';
+import delay = require('.');
+import {ClearablePromise} from '.';
 
-expectType<void>(await delay(200));
+expectType<ClearablePromise<void>>(delay(200));
 
-expectType<string>(await delay(200, {value: '🦄'}));
-expectType<number>(await delay(200, {value: 0}));
+expectType<ClearablePromise<string>>(delay(200, {value: '🦄'}));
+expectType<ClearablePromise<number>>(delay(200, {value: 0}));
+expectType<ClearablePromise<void>>(
+	delay(200, {signal: new AbortController().signal})
+);
 
-expectType<never>(await delay.reject(200, {value: '🦄'}));
-expectType<never>(await delay.reject(200, {value: 0}));
+expectType<ClearablePromise<never>>(delay.reject(200, {value: '🦄'}));
+expectType<ClearablePromise<never>>(delay.reject(200, {value: 0}));
 
-const customDelay = delay.createWithTimers({clearTimeout, setTimeout})
-expectType<void>(await customDelay(200));
+const customDelay = delay.createWithTimers({clearTimeout, setTimeout});
+expectType<ClearablePromise<void>>(customDelay(200));
 
-expectType<string>(await customDelay(200, {value: '🦄'}));
-expectType<number>(await customDelay(200, {value: 0}));
+expectType<ClearablePromise<string>>(customDelay(200, {value: '🦄'}));
+expectType<ClearablePromise<number>>(customDelay(200, {value: 0}));
 
-expectType<never>(await customDelay.reject(200, {value: '🦄'}));
-expectType<never>(await customDelay.reject(200, {value: 0}));
+expectType<ClearablePromise<never>>(customDelay.reject(200, {value: '🦄'}));
+expectType<ClearablePromise<never>>(customDelay.reject(200, {value: 0}));
